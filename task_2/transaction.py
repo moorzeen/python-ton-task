7ba79af9801baffe57e3aaaa87cf946aed54f7c0
Alexey <moorzeen@icloud.com>
2024-08-23 15:15:41 +0700

    task 2

--- task_2/transaction.py
+++ task_2/transaction.py
@@ -4,7 +4,7 @@ from pytoniq_core import WalletMessage
 from seed import mnemo
 
 async def transfer():
-    async with LiteBalancer.from_testnet_config(trust_level=2) as client:
+    async with LiteBalancer.from_mainnet_config(trust_level=2) as client:
         wallet = await WalletV4R2.from_mnemonic(client, mnemo)
         print(wallet, wallet.balance)
 
@@ -14,7 +14,7 @@ async def transfer():
 
         msg = wallet.create_wallet_internal_message(
             destination=Address("kQBOrhNyLCPNnt7MUVxJVUSEUQF6_10q7plprvRwCiqYhJF9"),
-            send_mode=128,
+            send_mode=17,
             value=1 * 10 ** 7,
             body=body
         )
