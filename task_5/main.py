4434c733c3e37824a0f011b832e508ac85f64af7
Alexey <moorzeen@icloud.com>
2024-08-26 23:59:31 +0700

    task 5

--- task_5/main.py
+++ task_5/main.py
@@ -4,13 +4,6 @@ from bitstring import BitArray
 from pytoniq import LiteBalancer, WalletV4R2
 from pytoniq_core.crypto.keys import mnemonic_new
 
-
-def get_prefix(binary_string: str) -> str:
-    reversed_binary = binary_string[::-1]
-    first_1_index_reversed = reversed_binary.find('1') + 1
-    prefix_reversed = reversed_binary[first_1_index_reversed:]
-    return prefix_reversed[::-1]
-
 async def main():
     async with LiteBalancer.from_mainnet_config(trust_level=2) as client:
         while True:
